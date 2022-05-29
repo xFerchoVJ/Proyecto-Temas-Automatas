388665f294d6a4e420f0a072a067fc94315408c6
xFerchoVJ <fer_elsword@hotmail.com>
2022-05-29 02:18:16 -0500

    Añadiendo DATA static
    Los datos para mostrar en la pagina se añadieron de forma que
    pueda usarlos como un objecto y podrer mostrarlo más facil y
    de mejor manera.

--- src/components/Gramaticas.jsx
+++ src/components/Gramaticas.jsx
@@ -1,10 +1,32 @@
-import { ButtonBack } from "./ButtonBack"
-
+import { gramatica } from "../data/data";
+import { ButtonBack } from "./ButtonBack";
+console.log(gramatica)
 export const Gramaticas = () => {
   return (
-    <div className="container">
-      <h1 className="text-center pt-5">Gramáticas</h1>
-      <ButtonBack/>
+    <div className="container-md">
+      <h1 className="text-center fw-bold pt-5">Gramáticas</h1>
+      <div className="row">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/F8ZWzQjHU-c"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="col-12"
+        ></iframe>
+        <h2 className="col-12">
+          En el campo de la informática, el concepto de Gramática Formal
+          adquirió gran importancia para el desarrollo de lenguajes de
+          programación.
+        </h2>
+        <h3 className="col-12 fw-bold">{gramatica.simboloTitulo}: </h3>
+        <p className="col-12 fs-3 ">{gramatica.simboloTexto}</p>
+      </div>
+      <div className="d-grid pt-5">
+        <ButtonBack />
+      </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const gramatica = {
+  simboloTitulo: "SIMBOLO",
+  simboloTexto: `Es una entidad abstracta, que no se va a definir. Normalmente los símbolos son letras (a,b,c,…z), dígitos (0,1,2…9) y otros caracteres (+,*,/,-,?...).
+  Un símbolo también puede estar formado por varias letras o caracteres, como las palabras reservadas de un lenguaje de programación son símbolos de dicho lenguaje. Ejemplo:
+  -   a,b,c,#,+,-,*, then, begin, end, else, …` 
+}
